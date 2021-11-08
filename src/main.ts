da3444a09804c9a966e58214e1fd8ac8c39e4373
diansigit <diansigit.p@gmail.com>
2021-11-08 11:08:14 +0700

    First Initialize

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('API UIIOrganisasi')
+    .setDescription('Dokumentasi API master data atau master referensi UIIOrganisasi')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
